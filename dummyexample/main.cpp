33b342937e071e4249f36464a9e2985503b7da64
Pankhuri Vanjani <pankhurivanjani@gmail.com>
2019-06-07 21:08:52 +0530

    Add files via upload

---
+++ dummyexample/main.cpp
@@ -0,0 +1,21 @@
+
+#include <sstream>
+
+#ifndef ROS1_H
+    #define ROS1_H
+    #include "ROS1_H.hpp"
+#endif //ROS1_H
+#ifndef ROS2_H
+    #define ROS2_H
+    #include "ROS2_H.hpp"
+#endif//ROS2_H
+
+int main(int argc, char **argv)
+{
+    printf("Hello World!");
+
+}
+
+
+
+
