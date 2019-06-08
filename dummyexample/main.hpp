d52c5638367d75a54478fcf397afead882cb11c8
Pankhuri Vanjani <pankhurivanjani@gmail.com>
2019-06-08 15:19:46 +0530

    Create main.hpp

---
+++ dummyexample/main.hpp
@@ -0,0 +1,11 @@
+#ifndef ROS1_H
+    #define ROS1_H
+    #include "ros/ros.h"
+    #include "std_msgs/String.h"
+#endif
+#ifndef ROS2_H
+    #define ROS2_H
+    #include "rclcpp/rclcpp.hpp"
+    #include "std_msgs/msg/string.hpp"       
+#endif
+
