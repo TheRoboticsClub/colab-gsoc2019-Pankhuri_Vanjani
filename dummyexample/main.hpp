b0a4ba250fe570b8fff2b0bdb2902430715d7fe1
Pankhuri Vanjani <pankhurivanjani@gmail.com>
2019-06-11 16:18:28 +0530

    Update main.hpp

--- dummyexample/main.hpp
+++ dummyexample/main.hpp
@@ -1,10 +1,8 @@
-#ifndef ROS1_H
-    #define ROS1_H
-    #include "ros/ros.h"
-    #include "std_msgs/String.h"
+#ifdef ROS1_H
+   #include "ros/ros.h"
+   #include "std_msgs/String.h"   
 #endif
-#ifndef ROS2_H
-    #define ROS2_H
+#ifdef ROS2_H
     #include "rclcpp/rclcpp.hpp"
     #include "std_msgs/msg/string.hpp"       
 #endif
